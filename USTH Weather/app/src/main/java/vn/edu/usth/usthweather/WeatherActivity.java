b7bcadc99c832c900d8da91f5aed8ed696b23632
datdt-026 <71490730+datdt-026@users.noreply.github.com>
2021-10-16 00:32:19 +0700

    update labwork 7 and 8

--- USTH Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ USTH Weather/app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -1,9 +1,17 @@
 package vn.edu.usth.usthweather;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
     public static final String TAG = WeatherActivity.class.getSimpleName();
@@ -14,15 +22,24 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         // Create a new Fragment to be placed in the activity layout
-        ForecastFragment ff =  ForecastFragment.newInstance("","");
-        // Add the fragment to the 'container' FrameLayout
-        getSupportFragmentManager().beginTransaction().add(
-                R.id.ff, ff).commit();
+//        ForecastFragment ff =  ForecastFragment.newInstance("","");
+//        // Add the fragment to the 'container' FrameLayout
+//        getSupportFragmentManager().beginTransaction().add(
+//                R.id.ff, ff).commit();
+//
+//        WeatherFragment wf =  WeatherFragment.newInstance("","");
+//        // Add the fragment to the 'container' FrameLayout
+//        getSupportFragmentManager().beginTransaction().add(
+//                R.id.wf, wf).commit();
 
-        WeatherFragment wf =  WeatherFragment.newInstance("","");
-        // Add the fragment to the 'container' FrameLayout
-        getSupportFragmentManager().beginTransaction().add(
-                R.id.wf, wf).commit();
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(
+                getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.header);
+        tabLayout.setupWithViewPager(pager);
 
         Log.i(TAG, "onStart");
     }
@@ -59,4 +76,31 @@ public class WeatherActivity extends AppCompatActivity {
         super.onDestroy();
         Log.i(TAG, "onDestroy");
     }
+
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] { "Hanoi", "Paris", "Toulouse" };
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return PAGE_COUNT; // number of pages for a ViewPager
+        }
+        @Override
+        public Fragment getItem(int page) {
+// returns an instance of Fragment corresponding to the specified page
+            switch (page) {
+                case 0: return new  WeatherForecastFragment();
+                case 1: return new  WeatherForecastFragment();
+                case 2: return new  WeatherForecastFragment();
+            }
+            return new Fragment(); // failsafe
+        }
+        @Override
+        public CharSequence getPageTitle(int page) {
+            return titles[page];
+        }
+    }
+
 }
